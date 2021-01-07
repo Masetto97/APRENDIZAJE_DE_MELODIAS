a3ac587913db65d11f218cb65dab00fc23ad5dde
Masetto97 <eduardo.mora.glez@gmail.com>
2021-01-07 12:02:57 +0100

    prueba bbdd

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, flash, redirect, url_for, render_template, request
 from datetime import datetime
-#from flask_mysqldb import MySQL
+import mariadb
+import json
 
 app = Flask(__name__)
 
@@ -16,6 +17,14 @@ app.secret_key = 'clave_secreta_flask'
 #mysql = MySQL(app)
 
 # context processors
+
+config = {
+    'host': '127.0.0.1',
+    'port': 3306,
+    'user': 'root',
+    'password': 'root',
+    'database': 'TFM'
+}
 
 @app.context_processor
 def date_now():
@@ -40,6 +49,28 @@ def biblioteca():
 @app.route("/cargar")
 def cargar():
                 return render_template('cargar.html') 
+
+
+@app.route('/api/people', methods=['GET'])
+def index():
+   # connection for MariaDB
+   conn = mariadb.connect(**config)
+   # create a connection cursor
+   cur = conn.cursor()
+   # execute a SQL statement
+   cur.execute("select * from USUARIOS")
+
+   # serialize results into JSON
+   row_headers=[x[0] for x in cur.description]
+   rv = cur.fetchall()
+   json_data=[]
+   for result in rv:
+        json_data.append(dict(zip(row_headers,result)))
+
+   # return the results!
+   return json.dumps(json_data)
+
+
 
 if __name__ == "__main__":
        app.run(debug=True)   
