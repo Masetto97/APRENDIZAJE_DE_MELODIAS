e2ea32fb669769d66607488d1c2768a4bfed9c5d
Masetto97 <eduardo.mora.glez@gmail.com>
2021-04-13 18:45:18 +0200

    prueba subida archivo

--- main.py
+++ main.py
@@ -14,13 +14,14 @@ app.secret_key = 'clave_secreta_flask'
 #Carpeta para subir los archivo antes de guardarlos en la BBDD o procesarlos
 UPLOAD_FOLDER = 'Upload' # /ruta/a/la/carpeta
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 #Usuario Actual
 ID_USUARIO_ACTUAL = 0
 
 # configuration used to connect to MariaDB
 config = {
-    'host': 'mariadb',
+    'host': '172.18.0.2',
     'port': 3306,
     'user': 'user',
     'password': 'password',
@@ -137,19 +138,55 @@ def subir():
     # Output message if something goes wrong...
     msg = ''
     # Check if "username", "password" and "email" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'ruta' in request.form:# and 'titulo' in request.form and 'ruta' in request.form:
+    if request.method == 'POST':# and 'titulo' in request.form and 'ruta' in request.form:
         # Create variables for easy access
         #titulo = request.form['titulo']
-        ruta = request.files['ruta']
- 
-
+        print('-------------------------------------->-------------------------------------')
+        ruta = request.files['RUTA']
+        msg = 'Registro Exitoso!'
+        print(ruta)
         filename = secure_filename(ruta.filename)
         send_from_directory(os.path.join(os.getcwd(), app.config["UPLOAD_FOLDER"]),filename,as_attachment=True,)
         #ruta.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        
+        print(os.path.join(os.getcwd(), app.config["UPLOAD_FOLDER"]))
         msg = 'Registro Exitoso!'
+    else: 
+      msg = 'ELSE'
     # Show registration form with message (if any)
     return render_template('subir.html', msg=msg) 
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return  '''<h1>Upload File</h1>'''
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
 
 
 @app.route('/logout')
@@ -157,4 +194,5 @@ def logout():
    return redirect('/') 
 
 if __name__ == "__main__":
-       app.run(debug=True, host='0.0.0.0')   
+       #app.run(debug=True, host='0.0.0.0')   
+       app.run(debug=True) 
