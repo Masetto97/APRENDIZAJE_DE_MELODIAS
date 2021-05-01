61e1944182332eb4b1d6d6bd51b7b84ffc1f88b2
Masetto97 <eduardo.mora.glez@gmail.com>
2021-05-01 15:17:09 +0200

    prueba

--- main.py
+++ main.py
@@ -169,7 +169,7 @@ def biblioteca():
     conn = mariadb.connect(**config)
     cursor = conn.cursor()
 
-    cursor.execute('SELECT * FROM CANCION WHERE Usuario = %s', (CURRENT_USER_ID))
+    cursor.execute('SELECT * FROM CANCION')
 
     songs = cursor.fetchall()
 
